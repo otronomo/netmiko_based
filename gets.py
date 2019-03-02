14b63feb612620ede9052e60e451f1924bd71836
NEG <aalex1055@gmail.com>
2019-03-02 15:59:43 +0100

    Added get_ip_int_bri_ios

--- gets.py
+++ gets.py
@@ -8,34 +8,44 @@ from pprint import pprint
 import re
 import sys
 
-
+# Retruns formatted ARP table. Takes VRF="" as arg:
+# LIST of DICTS 
 def get_arp_ios(host_dict, vrf=""):
     hostname = host_dict['host']
     try:
     # Establish connection
-        print("Trying connection to " + hostname)
+        #print("Trying connection to " + hostname)
         connection = Netmiko(**host_dict)
     except:
         print("get_arp_ios - Could not establish ssh connection to host" + hostname)
         return -1
     # Run command with textfsm - this should return structured data
-    print("Running command")
+    #print("Running command")
     if vrf == "":
         output = connection.send_command("sh ip arp", use_textfsm = True)
     else:
         output = connection.send_command("sh ip arp vrf " + vrf, use_textfsm = True)
     # Return structured data
-    print("Returning output")
+    #print("Returning output")
     return output
-
-host1 = {
-    'host': '10.20.20.100',
-    'username': 'cisco',
-    'password': 'cisco',
-    'device_type': 'cisco_ios',
-}
-
-
+    
+# Retruns formatted sh ip int brie table:
+# LIST of DICTS     
+def get_ip_int_bri_ios(host_dict):
+    hostname = host_dict['host']
+    try:
+    # Establish connection
+        #print("Trying connection to " + hostname)
+        connection = Netmiko(**host_dict)
+    except:
+        print("get_ip_int_bri_ios - Could not establish ssh connection to host" + hostname)
+        return -1
+    # Run command with textfsm - this should return structured data
+    #print("Running command")
+    output = connection.send_command("sh ip int brie", use_textfsm = True)
+    # Return structured data
+    #print("Returning output")
+    return output
 
 
 
