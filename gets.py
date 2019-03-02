5185a6c5fb7c2340d8c08c37ca6b445a60b2e844
NEG <aalex1055@gmail.com>
2019-03-02 15:36:23 +0100

    Cleanup

--- gets.py
+++ gets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # By NOMO
 
-<<<<<<< HEAD
+
 from netmiko import Netmiko
 from getpass import getpass
 from pprint import pprint
@@ -36,8 +36,6 @@ host1 = {
 }
 
 
-=======
->>>>>>> 763944c8be58268fb9196688b169421ab6dfa6ea
 
 
 
