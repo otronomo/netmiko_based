1282cbfdf702dabf8cda46bcd2f642caefaf5743
NEG <aalex1055@gmail.com>
2019-03-04 13:28:30 +0100

    Added get_ver_ios

--- gets.py
+++ gets.py
@@ -8,6 +8,20 @@ from pprint import pprint
 import re
 import sys
 
+'''
+All of this functions take at least the host_dict parameter.
+This is expected to be a dict just like this:
+
+host1 = {
+    'host': '10.20.20.100',
+    'username': 'cisco',
+    'password': 'cisco',
+    'device_type': 'cisco_ios',
+}
+
+'''
+
+
 # Retruns formatted ARP table. Takes VRF="" as arg:
 # LIST of DICTS 
 def get_arp_ios(host_dict, vrf=""):
@@ -82,6 +96,25 @@ def get_cdp_neig_ios(host_dict):
     # Run command with textfsm - this should return structured data
     #print("Running command")
     output = connection.send_command("sh cdp neig", use_textfsm = True)
+    # Return structured data
+    #print("Returning output")
+    return output
+
+
+# Returns formatted sh ver
+# LIST of 1 DICT element
+def get_ver_ios(host_dict):
+    hostname = host_dict['host']
+    #print("Trying connection to " + hostname)
+    try:
+    # Establish connection
+        connection = Netmiko(**host_dict)
+    except:
+        print("get_sh_ver_ios - Could not establish ssh connection to host" + hostname)
+        return -1
+    # Run command with textfsm - this should return structured data
+    #print("Running command")
+    output = connection.send_command("sh ver", use_textfsm = True)
     # Return structured data
     #print("Returning output")
     return output
